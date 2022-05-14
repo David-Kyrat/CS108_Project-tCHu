97a66bf3e7ec6cca20139a537407b5bf86899811
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-05-14 22:25:31 +0200

    gigitnored comply bugged

--- Tchu_Jar_Executable/run-tchu.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './files/run-tchu.ps1'" 
-
--- Tchu_Jar_Executable/tmp/run-tchu.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-int main(void) {
-  system("set pt=\%CD\%");
-  system("cd files && run-tchu.bat");
- // system("run-tchu.bat");
-  system("cd \%pt\%");
-  return 0;
-}
